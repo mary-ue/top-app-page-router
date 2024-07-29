f9a12ea449ef57e8a012157439818e84b45a69b6
mary-ue <maryandreeva100@gmail.com>
2024-07-29 20:12:44 +0300

    add fonts and color's variables

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,11 +18,16 @@ class MyDocument extends Document {
     return (
       <html lang="ru">
         <Head>
-          <body>
-            <Main />
-            <NextScript />
-          </body>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </html>
     );
   }
