f2517f106500919f2f85119ba793c1eb991abba1
mary-ue <maryandreeva100@gmail.com>
2024-07-30 20:58:11 +0300

    component P

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Htag } from "../components";
+import { Button, Htag, P } from "../components";
 
 export default function Home(): JSX.Element {
 	
@@ -7,6 +7,9 @@ export default function Home(): JSX.Element {
 			<Htag tag='h1'>Текст</Htag>
 			<Button appearance="primary" arrow="right">Button</Button>
 			<Button appearance="ghost" arrow="down">Button</Button>
+			<P size='l'>Большой</P>
+			<P>Средний</P>
+			<P size='s'>Маленький</P>
 		</div>
 	);
 }
