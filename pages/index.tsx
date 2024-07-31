27cd5d838489b04a8e915e54cc3a2e59156182e9
mary-ue <maryandreeva100@gmail.com>
2024-08-01 01:41:09 +0300

    Rating component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,34 @@
-import { Button, Htag, P, Tag } from "../components";
+import { useState } from 'react';
+import { Button, Htag, P, Rating, Tag } from '../components';
 
 export default function Home(): JSX.Element {
-	
-	return (
-		<div>
-			<Htag tag='h1'>Текст</Htag>
-			<Button appearance="primary" arrow="right">Button</Button>
-			<Button appearance="ghost" arrow="down">Button</Button>
-			<P size='l'>Большой</P>
-			<P>Средний</P>
-			<P size='s'>Маленький</P>
-			<Tag size='s'>Ghost</Tag>
-			<Tag size='m' color='red'>Red</Tag>
-			<Tag size='s' color='green'>Green</Tag>
-			<Tag color='primary'>Green</Tag>
-		</div>
-	);
+	const [rating, setRating] = useState<number>(4);
+
+  return (
+    <div>
+      <Htag tag="h1">Text</Htag>
+      <Button
+        appearance="primary"
+        arrow="right"
+      >
+        Button
+      </Button>
+      <Button appearance="ghost" arrow="down">
+        Button
+      </Button>
+      <P size="l">Большой</P>
+      <P>Средний</P>
+      <P size="s">Маленький</P>
+      <Tag size="s">Ghost</Tag>
+      <Tag size="m" color="red">
+        Red
+      </Tag>
+      <Tag size="s" color="green">
+        Green
+      </Tag>
+      <Tag color="primary">Green</Tag>
+			<Rating rating={4} />
+			<Rating rating={rating} isEditable setRating={setRating} />
+    </div>
+  );
 }
