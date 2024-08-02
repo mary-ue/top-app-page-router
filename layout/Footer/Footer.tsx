028ada13454049e99da194fd3172c10624a782b4
mary-ue <maryandreeva100@gmail.com>
2024-08-03 02:07:39 +0300

    complete footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,6 +1,15 @@
+import cn from 'classnames';
 import styles from './Footer.module.css';
 import { FooterProps } from './Footer.props';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <div className={cn(className, styles.footer)} {...props}>
+      <div>
+				OwlTop © 2024 - {format(new Date(), 'yyyy')} Все права защищены
+			</div>
+			<a href="#" target="_blank">Пользовательское соглашение</a>
+			<a href="#" target="_blank">Политика конфиденциальности</a>
+    </div>);
 };
