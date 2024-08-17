ed51b40269760708c54f8ef64d1d7f0522b5f883
mary-ue <maryandreeva100@gmail.com>
2024-08-18 00:14:03 +0300

    product page

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,4 +1,4 @@
-import { HhData, Htag, Tag } from '../../components';
+import { Advantages, HhData, Htag, P, Tag } from '../../components';
 import { TopPageComponentProps } from './TopPageComponent.props';
 import styles from './TopPageComponent.module.css';
 import { TopLevelCategory } from '../../interfaces/page.interface';
@@ -20,13 +20,30 @@ export const TopPageComponet = ({
         <span>Sort</span>
       </div>
       <div>
-        {products && products.map(p => (<div key={p._id}>{p.title}</div>))}
+        {products && products.map((p) => <div key={p._id}>{p.title}</div>)}
       </div>
       <div className={styles.hhTitle}>
         <Htag tag="h2">Вакансии - {page.category}</Htag>
-        <Tag color="red" size="m">hh.ru</Tag>
+        <Tag color="red" size="m">
+          hh.ru
+        </Tag>
       </div>
-      {firstCategory === TopLevelCategory.Courses && <HhData {...page.hh} />}
+      {firstCategory === TopLevelCategory.Courses && page.hh && (
+        <HhData {...page.hh} />
+      )}
+      {page.advantages && page.advantages.length > 0 && (
+        <>
+          <Htag tag="h2">Преимущства</Htag>
+          <Advantages advantages={page.advantages} />
+        </>
+      )}
+      {page.seoText && <P>{page.seoText}</P>}
+      <Htag tag="h2">Получаемые навыки</Htag>
+      {page.tags.map((t) => (
+        <Tag key={t} color="primary">
+          {t}
+        </Tag>
+      ))}
     </div>
   );
 };
